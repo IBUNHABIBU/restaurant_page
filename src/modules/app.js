c4202fc52cd93faf255c8831412279862e006996
IBUNHABIBU <ibunhabib@gmail.com>
2020-08-04 17:27:45 +0300

    correcting the lint errors

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,6 +1,9 @@
 import loadMainPage from './main';
+
 import createMenu from './menu';
+
 import createContact from './contact';
+
 const navbar = () => {
   const menu = document.getElementById('myMenu');
   menu.addEventListener('click', createMenu);
